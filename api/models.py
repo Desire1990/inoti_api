eb45f277972ce79776d1870005284ad8955b3fae
Desos <desnduwim26@gmail.com>
2022-01-06 14:47:19 +0200

    models and urls registered

---
+++ api/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+
+from datetime import datetime, timedelta, date
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class LastLogin(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+	date = models.DateTimeField(default=timezone.now, editable=False)
+
+class Client(models.Model):
+	nom = models.CharField(max_length=64, null=True)
+	montant = models.FloatField(default=0)
+	tel = models.CharField(max_length=20, unique = True)
+
+	class Meta:
+		unique_together = ('nom', 'tel')
+
+	def __str__(self):
+		return f"{self.nom} {self.tel}"
+
+class Account(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+	user = models.OneToOneField(User, on_delete = models.CASCADE )
+	montant = models.FloatField(default=0)
+
+
+
+class AccountCanada(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	montant = models.FloatField(default = 0)
+	transfer = models.FloatField(default=0)
+
+	def __str__(self):
+		return f"self.user.usename"
+
+class AccountBurundi(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	montant = models.FloatField(default = 0)
+	transfer = models.FloatField(default=0)
+
+	def __str__(self):
+		return f"self.user.usename"
+
+class Payment(models.Model):
+	id = models.SmallAutoField(primary_key = True)
+	client = models.ForeignKey(Client, on_delete = models.CASCADE)
+	montant = models.FloatField(default = 0)
+	data = models.DateTimeField(default=timezone.now, editable=False)
+
+
+class Provisioning(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+
+
+class Depense(models.Model):
+	id = models.SmallAutoField(primary_key=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	montant = models.FloatField(default = 0)
+	date = models.DateTimeField(default=timezone.now, editable = False)
+	is_valid = models.BooleanField(default = False)
+
+	def __str__(self):
+		return f"{self.user.username}"
+    
---
+++ api/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include, re_path
+from rest_framework import routers
+from .views import *
+
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+from dj_rest_auth.registration.views import RegisterView, VerifyEmailView, ConfirmEmailView
+from dj_rest_auth.views import LoginView, LogoutView
+
+
+router = routers.DefaultRouter()
+router.register("user",UserViewset)
+
+app_name='api'
+
+urlpatterns = [
+	path('', include(router.urls)),
+	path('api-auth/', include('rest_framework.urls')),
+	path('login/', TokenPairView.as_view()),
+	path('refresh/', TokenRefreshView.as_view()),
+    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(), name='account_confirm_email'),
+]
