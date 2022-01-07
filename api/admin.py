d35473bf73a945056c603355517ee0a57c9be9ff
Desos <desnduwim26@gmail.com>
2022-01-07 13:15:56 +0200

    models registered in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(LastLogin)
+admin.site.register(Client)
+admin.site.register(Account)
+admin.site.register(Depot)
+admin.site.register(Transaction)
+admin.site.register(Depense)
+admin.site.register(Payment)
+admin.site.register(Provisioning)
