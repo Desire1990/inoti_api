ef5267daa6e44eb8e6da2e373bcab95dfd71f5e9
Desos <desnduwim26@gmail.com>
2022-01-22 20:24:57 +0200

    admin changed

--- api/admin.py
+++ api/admin.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(LastLogin)
-admin.site.register(Client)
 admin.site.register(Account)
-admin.site.register(Depot)
+admin.site.register(Transfer)
 admin.site.register(Transaction)
 admin.site.register(Depense)
-admin.site.register(Payment)
 admin.site.register(Provisioning)
