ac33f0562045baa00fcb88dc10a64c5a15dfda08
Desos <desnduwim26@gmail.com>
2022-02-09 14:57:38 +0200

    taux registered

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(LastLogin)
+admin.site.register(Taux)
 admin.site.register(Account)
 admin.site.register(Transfer)
 admin.site.register(Depense)
